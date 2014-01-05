e5bfc714e4e5b1aad63ad86dcb91374c1a968bc3
Nicolas Ledez <github@ledez.net>
2014-01-05 23:05:07 +0100

    Add talk model tests

--- spec/models/talk_spec.rb
+++ spec/models/talk_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Talk do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+      :name => "Better apps with Rails",
+      :description => "Howto create an app with ROR"
+    }
+  end
+
+  it "should create a talk given a valid attribute" do
+    Talk.create!(@attr)
+  end
+
+  it "should require an name" do
+    no_name_talk = Talk.new(@attr.merge(:name => ""))
+    no_name_talk.should_not be_valid
+  end
+
+  it "should reject duplicate name" do
+    Talk.create!(@attr)
+    talk_with_duplicate_name = Talk.new(@attr)
+    talk_with_duplicate_name.should_not be_valid
+  end
 end
